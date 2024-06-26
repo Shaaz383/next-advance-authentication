08aeee1b5f336056d13ffac96a139b388d1c6bdd
Shaaz <asadakhtar800@gmail.com>
2024-06-26 16:42:58 +0530

    Register page design

--- schemas/index.ts
+++ schemas/index.ts
@@ -4,3 +4,8 @@ export const LoginSchema = z.object({
   email:z.string().email({message:"Email is required"}),
   password:z.string().min(1, {message : "Password is required"})
 })
+export const RegisterSchema = z.object({
+  email:z.string().email({message:"Email is required"}),
+  name:z.string().min(3,{message:"Name is required"}),
+  password:z.string().min(6, {message : "Password must be atleast 6 digit"})
+})
