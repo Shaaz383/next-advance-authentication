4d3961b604e06d56e907f512bf0c0d53c21ff463
Shaaz <asadakhtar800@gmail.com>
2024-06-28 14:12:37 +0530

    Add middleware and login functionality

--- auth.ts
+++ auth.ts
@@ -1,10 +1,17 @@
 import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
-// import Google from "next-auth/providers/google"
+import authConfig from "./auth.config"
+import { db } from "./lib/db"
+import {PrismaAdapter} from "@auth/prisma-adapter";
+
+
  
 export const {
   handlers : {GET , POST},
   auth,
+  signIn,
+  signOut,
 } = NextAuth({
-  providers: [GitHub]
+  adapter: PrismaAdapter(db),
+  session: {strategy : "jwt"},
+  ...authConfig
 })
--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,36 @@
-import {auth} from "./auth"
+import NextAuth from "next-auth";
+import authConfig from "./auth.config";
+const {auth} = NextAuth(authConfig)
 
-export default auth((req)=>{
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  apiAuthPrefix,
+  authRoutes,
+  publicRoutes,
+} from "@/routes";
+
+
+export default auth((req) => {
+  const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
-  console.log("ROUTE" , req.nextUrl.pathname);
-  console.log("IS LOGGED IN " , isLoggedIn)
-})
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) return null;
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return null;
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+      return Response.redirect(new URL("/auth/login", nextUrl));
+  }
+});
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
