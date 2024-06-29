f9daba2b8b053e26316584303dba6f1a9b7ca10b
Shaaz <asadakhtar800@gmail.com>
2024-06-29 12:47:29 +0530

    Send Verification email by using resendemail service

--- actions/register.ts
+++ actions/register.ts
@@ -3,11 +3,15 @@
 
 import bcrypt from "bcryptjs"
 
+
 import * as z from "zod";
 
 import { RegisterSchema } from "@/schemas";
 import { db } from "@/lib/db";
 import { getUserByEmail } from "@/data/user";
+import { generateVerificationToken } from "@/lib/tokens";
+import { sendVerificationEmail } from "@/lib/mail";
+
 
 export const register = async(values: z.infer<typeof RegisterSchema>) =>{
   const validatedFields = RegisterSchema.safeParse(values);
@@ -35,6 +39,9 @@ export const register = async(values: z.infer<typeof RegisterSchema>) =>{
 
   //TODO Send verification token
 
+  const verificationToken = await generateVerificationToken(email);
+  await sendVerificationEmail(verificationToken.email, verificationToken.token);
 
-  return {success: "Register Successfully"}
+
+  return {success: "Confirmation email sent! "}
 }
