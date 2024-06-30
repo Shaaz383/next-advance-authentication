2abf97739ea1ef340d86774f1f186e85834ee45d
Shaaz <asadakhtar800@gmail.com>
2024-06-30 16:51:13 +0530

    solve error

--- auth.ts
+++ auth.ts
@@ -20,7 +20,7 @@ export const {
   auth,
   signIn,
   signOut,
-  update,
+  
 } = NextAuth({
   pages: {
     signIn: "/auth/login",
@@ -35,76 +35,79 @@ export const {
     },
   },
 
-  callbacks: {
 
+
+  callbacks: {
     async signIn({ user, account }) {
       // Allow OAuth without email verification
       if (account?.provider !== "credentials") return true;
-
+  
+      if (!user.id || typeof user.id !== 'string') {
+        return false; // or handle the error appropriately
+      }
+  
       const existingUser = await getUserById(user.id);
-
+  
       // Prevent sign in without email verification
       if (!existingUser?.emailVerified) return false;
-
+  
       if (existingUser.isTwoFactorEnabled) {
         const twoFactorConfirmation = await getTwoFactorConfirmationByUserId(
           existingUser.id
         );
-
+  
         if (!twoFactorConfirmation) return false;
-
+  
         // Delete two factor confirmation for next sign in
         await db.twoFactorConfirmation.delete({
           where: { id: twoFactorConfirmation.id },
         });
       }
-
+  
       return true;
     },
-
-
-
   
     async session({ token, session }) {
       if (token.sub && session.user) {
         session.user.id = token.sub;
       }
-
+  
       if (token.role && session.user) {
         session.user.role = token.role as UserRole;
       }
-
-
+  
       if (session.user) {
         session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
-        session.user.name = token.name;
-        session.user.email = token.email;
+        session.user.name = token.name ?? ''; // Provide a default value if token.name is null or undefined
+        session.user.email = token.email ?? ''; // Provide a default value if token.email is null or undefined
         session.user.isOAuth = token.isOAuth as boolean;
       }
-
+  
       return session;
     },
-
-
-
+  
     async jwt({ token }) {
       if (!token.sub) return token;
-
+  
       const existingUser = await getUserById(token.sub);
-
+  
       if (!existingUser) return token;
-
+  
       const existingAccount = await getAccountByUserId(existingUser.id);
-
+  
       token.isOAuth = !!existingAccount;
       token.name = existingUser.name;
       token.email = existingUser.email;
       token.role = existingUser.role;
       token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
-
+  
       return token;
     },
   },
+  
+
+
+  
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
